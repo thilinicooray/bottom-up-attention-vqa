da506a25ff89472d4ff25c4731b2eba6e0f395d1
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-04-09 17:55:39 +0800

    try gated tab

--- fc.py
+++ fc.py
@@ -3,11 +3,11 @@ import torch.nn as nn
 from torch.nn.utils.weight_norm import weight_norm
 
 
-class FCNet(nn.Module):
+class FCNetReLU(nn.Module):
     """Simple class for non-linear fully connect network
     """
     def __init__(self, dims):
-        super(FCNet, self).__init__()
+        super(FCNetReLU, self).__init__()
 
         layers = []
         for i in range(len(dims)-2):
@@ -22,6 +22,29 @@ class FCNet(nn.Module):
 
     def forward(self, x):
         return self.main(x)
+
+class FCNet(nn.Module):
+    """Simple class for non-linear fully connect network with gated tangent as in paper
+    """
+    def __init__(self, dims):
+        super(FCNet, self).__init__()
+
+        in_dim = dims[0]
+        out_dim = dims[1]
+        self.first_lin = weight_norm(nn.Linear(in_dim, out_dim), dim=None)
+        self.tanh = nn.Tanh()
+        self.second_lin = weight_norm(nn.Linear(in_dim, out_dim), dim=None)
+        self.sigmoid = nn.Sigmoid()
+
+
+    def forward(self, x):
+
+        y_hat = self.tanh(self.first_lin(x))
+        g = self.sigmoid(self.second_lin(x))
+
+        y = y_hat * g
+
+        return y
 
 
 if __name__ == '__main__':
