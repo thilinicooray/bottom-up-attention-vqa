16b82585b32ad34e476b2a9a49d736ecca7c8ac6
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-04-23 01:01:40 +0800

    iter verb

--- main_grid_imsitu_verb_iter.py
+++ main_grid_imsitu_verb_iter.py
@@ -346,7 +346,7 @@ def main():
             opts.append({'params': model.q_emb.parameters()})
 
         #optimizer = torch.optim.Adamax(opts, lr=1e-3)
-        torch.optim.SGD(opts, lr=0.001, momentum=0.9)
+        optimizer = torch.optim.SGD(opts, lr=0.001, momentum=0.9)
 
     elif args.resume_training:
         print('Resume training from: {}'.format(args.resume_model))
@@ -358,14 +358,14 @@ def main():
         model_name = 'resume_all'
         utils_imsitu.set_trainable(model, True)
         #optimizer = torch.optim.Adamax(model.parameters(), lr=1e-3)
-        torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
     else:
         print('Training from the scratch.')
         model_name = 'train_full'
 
         utils_imsitu.set_trainable(model, True)
         #optimizer = torch.optim.Adamax(model.parameters(), lr=1e-3)
-        torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
     #utils_imsitu.set_trainable(model, True)
     #optimizer = torch.optim.Adamax(model.parameters(), lr=1e-3)
