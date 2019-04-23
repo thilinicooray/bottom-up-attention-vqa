6b24f7847f5605598da8f1aa3460dc6693628e46
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-04-23 17:58:44 +0800

    iter verb

--- attention.py
+++ attention.py
@@ -5,9 +5,10 @@ from fc import FCNet
 
 
 class Attention(nn.Module):
-    def __init__(self, v_dim, q_dim, num_hid):
+    def __init__(self, v_dim, q_dim, num_hid, dropout=0.2):
         super(Attention, self).__init__()
         self.nonlinear = FCNet([v_dim + q_dim, num_hid])
+        self.dropout = nn.Dropout(dropout)
         self.linear = weight_norm(nn.Linear(num_hid, 1), dim=None)
 
     def forward(self, v, q):
@@ -24,6 +25,7 @@ class Attention(nn.Module):
         q = q.unsqueeze(1).repeat(1, num_objs, 1)
         vq = torch.cat((v, q), 2)
         joint_repr = self.nonlinear(vq)
+        joint_repr = self.dropout(joint_repr)
         logits = self.linear(joint_repr)
         return logits
 
