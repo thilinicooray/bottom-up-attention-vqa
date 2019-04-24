61d37bfc783b750a17115a6f4d29cfb69577f117
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-04-24 12:48:53 +0800

    iter verb

--- main_grid_imsitu_transfer.py
+++ main_grid_imsitu_transfer.py
@@ -342,7 +342,10 @@ def main():
     optimizer = torch.optim.Adamax([
         {'params': model.classifier.parameters()},
         {'params': model.w_emb.parameters()},
-        {'params': model.q_emb.parameters()},
+        {'params': model.q_emb.parameters(), 'lr': 5e-4},
+        {'params': model.v_att.parameters(), 'lr': 1e-5},
+        {'params': model.q_net.parameters(), 'lr': 1e-5},
+        {'params': model.v_net.parameters(), 'lr': 1e-5},
     ], lr=1e-3)
 
     #utils_imsitu.set_trainable(model, True)
