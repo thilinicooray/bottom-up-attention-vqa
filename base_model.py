51a44788415fa75095d2c95bf4594135259e65aa
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-04-26 12:41:02 +0800

    iter role

--- base_model.py
+++ base_model.py
@@ -192,6 +192,7 @@ class BaseModelGrid_Imsitu_RoleIter(nn.Module):
         return: logits, not probs
         """
         losses = []
+        prev_rep = None
         for i in range(self.num_iter):
 
             img = v.expand(self.encoder.max_role_count,v.size(0), v.size(1), v.size(2))
@@ -209,6 +210,10 @@ class BaseModelGrid_Imsitu_RoleIter(nn.Module):
             q_repr = self.q_net(q_emb)
             v_repr = self.v_net(v_emb)
             joint_repr = q_repr * v_repr
+            if i != 0:
+                joint_repr = joint_repr + prev_rep
+                prev_rep = joint_repr
+                
             logits = self.classifier(joint_repr)
 
             role_label_pred = logits.contiguous().view(v.size(0), self.encoder.max_role_count, -1)
@@ -216,10 +221,10 @@ class BaseModelGrid_Imsitu_RoleIter(nn.Module):
             if self.training:
                 losses.append(self.calculate_loss(gt_verb, role_label_pred, labels))
 
-            #label_idx = torch.max(role_label_pred,-1)[1]
+            label_idx = torch.max(role_label_pred,-1)[1]
             #for gt labels
-            frame_idx = np.random.randint(3, size=1)
-            label_idx = labels[:,frame_idx,:].squeeze()
+            #frame_idx = np.random.randint(3, size=1)
+            #label_idx = labels[:,frame_idx,:].squeeze()
 
             verb_q_idx = self.encoder.get_detailed_roleq_idx(gt_verb, label_idx)
 
