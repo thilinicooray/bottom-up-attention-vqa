79fa6be4e29a46a89937a5b0afdda2d2aead78b3
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-06-03 17:33:08 +0800

    rerun role general clean

--- imsitu_encoder_alldata_imsitu.py
+++ imsitu_encoder_alldata_imsitu.py
@@ -549,6 +549,7 @@ class imsitu_encoder():
             role_dict = {}
             for j in range(len(current_role_list)):
                 label = self.label_list[current_labels[j]]
+                #remember to add 'UNK' as a key to labelid2nlword
                 label_name = self.all_words[self.labelid2nlword[label]] if label in self.labelid2nlword else label
                 role_dict[current_role_list[j].upper()] = label_name
 
