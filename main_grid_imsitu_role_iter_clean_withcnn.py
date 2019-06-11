fc49d24404b279103a956ec465e65616eeaafc2b
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-06-11 21:24:05 +0800

    role only final

--- main_grid_imsitu_role_iter_clean_withcnn.py
+++ main_grid_imsitu_role_iter_clean_withcnn.py
@@ -354,6 +354,7 @@ def main():
             raise Exception('[pretrained module] not specified')
         utils_imsitu.load_net(args.resume_model, [model])
         optimizer_select = 0
+        optimizer = torch.optim.Adamax(model.parameters(), lr=1e-3)
         model_name = 'resume_all'
     else:
         print('Training from the scratch.')
