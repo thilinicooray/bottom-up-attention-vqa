e2358f1359a903c609fd11deef7c2e14eecdf8a7
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-06-30 07:13:32 +0800

    verb with external ctx

--- base_model.py
+++ base_model.py
@@ -2130,12 +2130,12 @@ class BaseModelGrid_Imsitu_RoleVerbIter_General_With_CNN_ExtCtx(nn.Module):
 
             q_repr = self.q_net(q_emb)
             v_repr = self.v_net(v_emb)
-            joint_repr = q_repr * v_repr
+            joint_repr = q_repr * v_repr + ext_ctx
             if i != 0:
                 joint_repr = self.dropout(joint_repr) + prev_rep
             prev_rep = joint_repr
 
-            combo_rep = joint_repr + ext_ctx
+            combo_rep = joint_repr
 
             logits = self.classifier(combo_rep)
 
