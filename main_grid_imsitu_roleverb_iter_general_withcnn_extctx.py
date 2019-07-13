606a05fb269476b263bf44ad7eb1751da656494c
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-07-13 15:42:31 +0800

    role feat for both

--- main_grid_imsitu_roleverb_iter_general_withcnn_extctx.py
+++ main_grid_imsitu_roleverb_iter_general_withcnn_extctx.py
@@ -349,7 +349,6 @@ def main():
                 {'params': model.classifier.parameters()},
                 {'params': model.w_emb.parameters()},
                 {'params': model.resize_img_flat.parameters()},
-                {'params': model.combo_att_img.parameters()},
                 {'params': model.q_emb.parameters(), 'lr': 1e-4},
                 {'params': model.v_att.parameters(), 'lr': 5e-5},
                 {'params': model.q_net.parameters(), 'lr': 5e-5},
