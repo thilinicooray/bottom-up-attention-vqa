b7d40cb1bf0c9626aef5a2ffe9cb2cebd2b1e81d
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-07-14 00:49:17 +0800

    role feat for both

--- base_model.py
+++ base_model.py
@@ -2630,16 +2630,7 @@ class BaseModelGrid_Imsitu_RoleVerbIter_General_With_CNN_ExtCtx(nn.Module):
             role_rep_combo = torch.sum(role_rep, 1)
             ext_ctx = img_feat_flat * role_rep_combo
 
-            #calc angular cosine between ctx and org image to find the distance between two
-            cos_sim = self.cosine_sim(img_feat_flat, ext_ctx)
-            ang_distance = torch.div(torch.acos(cos_sim), math.pi)
-            #print(cos_sim.size(), ang_distance.size(), img_feat_flat.size())
-
-            # get the difference of these two as we want to add that info to context, so that important details of
-            # image did not contained in q can be included
-            projected_extra_infor_img = -(img_feat_flat * ang_distance.unsqueeze(1))
-
-            new_ctx = self.proj_cat_ctx(torch.cat([projected_extra_infor_img, ext_ctx], -1))
+            new_ctx = self.proj_cat_ctx(torch.cat([ext_ctx, img_feat_flat], -1))
 
 
 
