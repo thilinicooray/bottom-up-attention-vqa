ee8788c00ed35a84d2bd65a57f3d141f69ab23ff
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-07-19 13:05:23 +0800

    role feat for both

--- main_grid_imsitu_roleverb_iter_general_withcnn_extctx.py
+++ main_grid_imsitu_roleverb_iter_general_withcnn_extctx.py
@@ -380,7 +380,8 @@ def main():
         utils_imsitu.load_net(args.role_module, [model.role_module])
         utils_imsitu.set_trainable(model.role_module, False)
         opts = [{'params': model.classifier.parameters()},
-                {'params': model.context_shaper.parameters()},
+                {'params': model.context_shaper_ad.parameters()},
+                {'params': model.context_shaper_mul.parameters()},
                 {'params': model.non_linear_combinator.parameters()},
                 {'params': model.v_att.parameters()},
                 {'params': model.q_net.parameters()},
