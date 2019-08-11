fe174abfff230cebeedc6ff96dc9c80afeb21f5b
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-08-11 17:38:59 +0800

    role feat for both

--- main_grid_imsitu_role_iter_clean_withcnn_newmodel.py
+++ main_grid_imsitu_role_iter_clean_withcnn_newmodel.py
@@ -350,7 +350,6 @@ def main():
             {'params': model.v_att.parameters()},
             {'params': model.q_net.parameters()},
             {'params': model.v_net.parameters()},
-            {'params': model.context_adder.parameters()}
         ], lr=1e-3)
 
 
