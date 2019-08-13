a8a5e72419e54764a419c1bbc90ad8324d7c74c8
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-08-13 21:19:32 +0800

    role feat for both

--- base_model.py
+++ base_model.py
@@ -2158,7 +2158,7 @@ class MultiHeadedAttention(nn.Module):
         x = x.transpose(1, 2).contiguous() \
             .view(nbatches, -1, self.h * self.d_k)
 
-        print(self.attn[0,:,0])
+        print(self.attn[0,:,1])
 
         return self.linears[-1](x)
 
