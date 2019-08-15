b526f0415e2b047b9e1d76283a4dccefab8604d4
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-08-15 16:20:12 +0800

    role feat for both

--- base_model.py
+++ base_model.py
@@ -1787,7 +1787,7 @@ class BaseModelGrid_Imsitu_RoleIter_With_CNN_NewModel(nn.Module):
             withctx_expand = withctx_expand.transpose(0,1)
             added_img = torch.cat([withctx_expand, img], -1)
             added_img = added_img.contiguous().view(-1, added_img.size(-1))
-            added_img = torch.sigmoid(self.resize_ctx(added_img))
+            added_img = torch.sigmoid(self.Dropout_C(self.resize_ctx(added_img)))
             added_img = added_img.contiguous().view(v.size(0) * self.encoder.max_role_count, -1, added_img.size(-1))
 
             img = added_img * img
