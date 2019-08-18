02de7d62c1cb7d923ac662d6224e40796def0982
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-08-18 22:23:52 +0800

    role feat for both

--- base_model.py
+++ base_model.py
@@ -1502,7 +1502,7 @@ class BaseModelGrid_Imsitu_RoleIter_With_CNN_NewModel(nn.Module):
         #self.verb_classifier = verb_classifier
         self.encoder = encoder
         self.num_iter = num_iter
-        self.resize_ctx = weight_norm(nn.Linear(self.hidden_size + 512, 512))
+        self.resize_ctx = nn.Linear(self.hidden_size + 512, 512)
         self.l2_criterion = nn.MSELoss()
         self.Dropout_M = nn.Dropout(0.1)
         self.Dropout_Q = nn.Dropout(0.1)
