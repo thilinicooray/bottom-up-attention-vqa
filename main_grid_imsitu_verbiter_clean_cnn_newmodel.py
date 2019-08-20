405203a5687404f67a6308a5588bb9693d266c90
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-08-20 19:10:31 +0800

    role feat for both

--- main_grid_imsitu_verbiter_clean_cnn_newmodel.py
+++ main_grid_imsitu_verbiter_clean_cnn_newmodel.py
@@ -365,7 +365,7 @@ def main():
         #utils_imsitu.set_trainable(model.convnet, False)
         #{'params': model.convnet.parameters(), 'lr': 5e-5},
         optimizer = torch.optim.Adamax([
-            {'params': model.convnet.parameters(), 'lr': 5e-5},
+            {'params': model.convnet.parameters(), 'lr': 1e-4},
             {'params': model.verb_classifier.parameters()},
             {'params': model.classifier.parameters()},
             {'params': model.role_emb.parameters()},
